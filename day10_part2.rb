4df524ed47b0c6ad3ddcfcffc34f2ffe84f9f2f3
joe adcock <joeadcock@roguepod.com>
2020-12-11 11:03:14 -0500

    removing duplicate lines

--- day10_part2.rb
+++ day10_part2.rb
@@ -6,22 +6,19 @@ chains[0] = 1
 (0...adapters.size).each do |index|
   current = adapters[index]
 
+  chains[current] += (chains[current - 1])
+  chains[current] += (chains[current - 2])
+  chains[current] += (chains[current - 3])
+
   if adapters.include?(current + 1)
-    chains[current] += (chains[current - 1])
-    chains[current] += (chains[current - 2])
-    chains[current] += (chains[current - 3])
+    # do nothing
   elsif adapters.include?(current + 2)
-    chains[current] += (chains[current - 1])
-    chains[current] += (chains[current - 2])
-    chains[current] += (chains[current - 3])
     chains.delete(current - 2)
   else
-    chains[current] += chains[current - 1]
-    chains[current] += chains[current - 2]
-    chains[current] += chains[current - 3]
     chains.delete(current - 1)
     chains.delete(current - 2)
   end
+
   chains.delete(current - 3)
 
   puts chains
